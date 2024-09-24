45d755756fbc82d1a818323ccd45f52e634f06e1
MartasinH <lugom15122@gmail.com>
2024-09-23 23:47:46 -0600

    Datos

--- Unidad 1/Modulo2/2.6.1.9 Entradas y salidas simples.py
+++ Unidad 1/Modulo2/2.6.1.9 Entradas y salidas simples.py
@@ -1,7 +1,12 @@
+'''Autor:Marta Lugo López
+Fecha de entrega:19/sep/2024'''
+#Declarar variables
 a=2.0 
 b=3.0 
+#Operaciones
 print(a+b)
 print(a-b)
 print(a*b)
 print(a/b)
+#Salida
 print("\n¡Eso es todo, amigos!")
