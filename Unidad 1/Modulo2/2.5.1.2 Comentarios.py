97eca6388328c82519ffa781f999dd94094b14b0
MartasinH <lugom15122@gmail.com>
2024-09-23 23:46:41 -0600

    Datos

--- Unidad 1/Modulo2/2.5.1.2 Comentarios.py
+++ Unidad 1/Modulo2/2.5.1.2 Comentarios.py
@@ -1,13 +1,18 @@
+'''Autor:Marta Lugo López
+Fecha de entrega:19/sep/2024'''
+
+#Declarar vaariables
 hour = int (input ("Hora de inicio (horas) :") )
 ming = int (input ("Minuto de inicio (minutos): ") )
 dura = int (input ("Duración del evento (minutos): "))
 
-
+#Operaciones
 total = ming + dura
 cociente = total // 60
 residuo = total % 60
 hour += cociente
 
+#SALIDA
 #print（str（hour）+'：'+str(residuo))
 print (hour,':', residuo)
 #print (f" (hour): (residuo) ")
