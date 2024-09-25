395b3c0d6deb93e2ec0bf992f975b1785963a7db
MartasinH <lugom15122@gmail.com>
2024-09-25 16:15:08 -0600

    MODULO 3

--- Unidad 1/Modulo3/3.2.1.3  Lo esencial del bucle while - Adivina el número secreto.py
+++ Unidad 1/Modulo3/3.2.1.3  Lo esencial del bucle while - Adivina el número secreto.py
@@ -1,2 +1,23 @@
 '''Autor:Marta Lugo López
 Fecha de entrega:26/sep/2024'''
+
+secret_number = 777
+
+print(
+"""
++==================================+
+| ¡Bienvenido a mi juego, muggle!  |
+| Introduce un número entero       |
+| y adivina qué número he          |
+| elegido para ti.                 |
+| Entonces,                        |
+| ¿Cuál es el número secreto?      |
++==================================+
+""")
+
+user_number = int(input("Ingresa el número: "))
+
+while user_number != secret_number:
+    print("¡Ja, ja! ¡Estás atrapado en mi bucle!")
+    user_number = int(input("Ingresa el número nuevamente: "))
+print(secret_number, "¡Bien hecho, muggle! Eres libre ahora.")
