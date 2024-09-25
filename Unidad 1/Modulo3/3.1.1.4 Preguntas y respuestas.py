15b2326e0ef84c66f62f66c560804e16b92da536
MartasinH <lugom15122@gmail.com>
2024-09-25 00:19:28 -0600

    MODULO 3

---
+++ Unidad 1/Modulo3/3.1.1.4 Preguntas y respuestas.py
@@ -0,0 +1,33 @@
+'''Autor:Marta Lugo López
+Fecha de entrega:26/sep/2024'''
+
+#Entrada
+n = 100
+#Condicion y resultado
+#print (n < 0)
+#print (n > 0)
+
+#Entrada 
+n = int(input(" Dame el numero: "))
+#Condicion,salida
+print (n <= 0)
+
+#Entrada 
+n = int(input(" Dame el numero: "))
+#Condicion y resultado
+print (n < 0)
+
+#Entrada 
+n = int(input(" Dame el numero: "))
+#Condicion y resultado
+print (n > 0)
+
+#Entrada 
+n = int(input(" Dame el numero: "))
+#Condicion y resultado
+print (n > 0)
+
+#Entrada
+n = int(input(" Dame el numero: "))
+#Condicion y resultado
+print (n != 0)
---
+++ Unidad 1/Modulo3/3.2.1.14  Fundamentos del bucle while.py
@@ -0,0 +1,2 @@
+'''Autor:Marta Lugo López
+Fecha de entrega:26/sep/2024'''
