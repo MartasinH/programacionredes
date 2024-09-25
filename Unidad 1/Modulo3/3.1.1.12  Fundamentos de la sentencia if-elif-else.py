ea24d39cc1d3d87f0a1cc55011652e843bd031af
MartasinH <lugom15122@gmail.com>
2024-09-25 16:21:03 -0600

    MODULO 3

--- Unidad 1/Modulo3/3.1.1.12  Fundamentos de la sentencia if-elif-else.py
+++ Unidad 1/Modulo3/3.1.1.12  Fundamentos de la sentencia if-elif-else.py
@@ -1,2 +1,16 @@
 '''Autor:Marta Lugo López
 Fecha de entrega:26/sep/2024'''
+
+año = int(input("Introduce un año: "))
+
+if año < 1582:
+	print("No esta dentro del período del calendario Gregoriano")
+else:
+	if año % 4 != 0:
+		print("Año Común")
+	elif año % 100 != 0:
+		print("Año Bisiesto")
+	elif año % 400 != 0:
+		print("Año Común")
+	else:
+		print("Año Bisiesto")
