a430d72eec745db207976157f56464d23d581629
MartasinH <lugom15122@gmail.com>
2024-09-25 00:50:20 -0600

    MODULO 3

--- Unidad 1/Modulo3/3.1.1.11 Fundamentos de la sentencia if-else.py
+++ Unidad 1/Modulo3/3.1.1.11 Fundamentos de la sentencia if-else.py
@@ -1,2 +1,18 @@
 '''Autor:Marta Lugo López
 Fecha de entrega:26/sep/2024'''
+
+
+#Entrada
+ingreso = float(input("Introduce el ingreso anual: "))
+#Condicion
+if ingreso < 85528:
+	tax = ingreso * 0.18 - 556.02
+else:
+	tax = (ingreso - 85528) * 0.32 + 14839.02
+
+if tax < 0.0:
+	tax = 0.0
+
+tax = round(tax, 0)
+#Salida
+print("El impuesto es:", tax, "pesos")
