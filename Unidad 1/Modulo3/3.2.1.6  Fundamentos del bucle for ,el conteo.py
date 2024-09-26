c9936bbfdce058c2d073b6834b0b9bb1d9cbe057
MartasinH <lugom15122@gmail.com>
2024-09-25 18:41:58 -0600

    MODULO 3

--- Unidad 1/Modulo3/3.2.1.6  Fundamentos del bucle for ,el conteo.py
+++ Unidad 1/Modulo3/3.2.1.6  Fundamentos del bucle for ,el conteo.py
@@ -1,2 +1,13 @@
 '''Autor:Marta Lugo López
 Fecha de entrega:26/sep/2024'''
+
+import time
+# Escribe un bucle for que cuente hasta cinco.
+for second in range(1, 6):
+# Cuerpo del bucle: imprime el número de iteración del bucle y la palabra "Mississippi".
+    print(second, "Mississippi")
+# Cuerpo del bucle - usar: time.sleep (1)
+    time.sleep(1)
+# Escribe una función de impresión con el mensaje final.	
+print("¡Listos o no, ahí voy!")
+
