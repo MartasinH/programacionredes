faff599d50855b9c14c4b141637534cdb91ff129
MartasinH <lugom15122@gmail.com>
2024-09-25 19:17:33 -0600

    MODULO 3

--- Unidad 1/Modulo3/3.2.1.9  La sentencia break - Atascado en un bucle.py
+++ Unidad 1/Modulo3/3.2.1.9  La sentencia break - Atascado en un bucle.py
@@ -1,2 +1,12 @@
 '''Autor:Marta Lugo López
 Fecha de entrega:26/sep/2024'''
+
+while True:
+# Escribir código en el cuerpo del bucle.
+    word = input("¡Estás atrapado en un bucle infinito!\n Escribe la palabra secreta: ")
+# Poner una sentencia condicional.
+    if word == "chupacabra":
+# Salir del bucle aquí.
+        break
+# Imprimir el mensaje.
+print("¡Has dejado el bucle con éxito!")
