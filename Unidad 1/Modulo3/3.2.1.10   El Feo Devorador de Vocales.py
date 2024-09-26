c6696913b4b943117a1c481048d705c54f859cba
MartasinH <lugom15122@gmail.com>
2024-09-25 19:30:49 -0600

    MODULO 3

---
+++ Unidad 1/Modulo3/3.2.1.10   El Feo Devorador de Vocales.py
@@ -0,0 +1,15 @@
+
+'''Autor:Marta Lugo López
+Fecha de entrega:26/sep/2024'''
+
+
+# Indicar al usuario que ingrese una palabra
+user_word = print("Ingresa un nombre ")
+
+# y asignarlo a la variable user_word.
+for letter in user_word:
+    
+# Completa el cuerpo del bucle for.
+   #if not (letter == 'A' or letter =='E' or letter == #'I' or letter == 'O' or letter == 'U')
+     print(letter)
+   #  time.sleep(1)
