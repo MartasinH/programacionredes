0d252f18bb479256c2b186109ba6c3f7b894784d
MartasinH <lugom15122@gmail.com>
2024-09-26 18:44:25 -0600

    MODULO 3

--- Unidad 1/Modulo3/3.2.1.14  Fundamentos del bucle while.py
+++ Unidad 1/Modulo3/3.2.1.14  Fundamentos del bucle while.py
@@ -1,2 +1,16 @@
 '''Autor:Marta Lugo López
 Fecha de entrega:26/sep/2024'''
+
+blocks = int(input("Ingresa el número de bloques: "))
+
+height = 0
+bloques_usados = 0
+
+while bloques_usados <= blocks:
+  height += 1
+  bloques_usados += height
+  if bloques_usados > blocks:
+    height -= 1
+    break
+
+print("La altura de la pirámide:", height)
