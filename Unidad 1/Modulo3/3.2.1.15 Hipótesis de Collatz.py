a2ebca5c3a712f0939f809105bf9e23254119aa7
MartasinH <lugom15122@gmail.com>
2024-09-26 18:08:17 -0600

    MODULO 3

--- Unidad 1/Modulo3/3.2.1.15 Hipótesis de Collatz.py
+++ Unidad 1/Modulo3/3.2.1.15 Hipótesis de Collatz.py
@@ -1,2 +1,18 @@
 '''Autor:Marta Lugo López
 Fecha de entrega:26/sep/2024'''
+
+c0 = int(input("Ingresa c0: "))
+
+if c0 > 1:
+	steps = 0
+	while c0 != 1:
+		if c0 %2 != 0:
+			cnew = 3 * c0 + 1
+		else:
+			cnew = c0 // 2
+		print(c0)
+		c0 = cnew
+		steps += 1
+	print("pasos =",steps)
+else:
+	print("Valor de c0 incorrecto")
