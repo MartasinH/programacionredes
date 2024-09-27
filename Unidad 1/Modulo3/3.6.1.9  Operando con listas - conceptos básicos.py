3c3b3ea9a3f2ed912fabd68b7f56f09c85d226b8
MartasinH <lugom15122@gmail.com>
2024-09-26 18:46:27 -0600

    MODULO 3

--- Unidad 1/Modulo3/3.6.1.9  Operando con listas - conceptos básicos.py
+++ Unidad 1/Modulo3/3.6.1.9  Operando con listas - conceptos básicos.py
@@ -1,3 +1,14 @@
 '''Autor:Marta Lugo López
 Fecha de entrega:26/sep/2024'''
 
+my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+
+aux = set()
+lista = []
+for num in my_list:
+    if num not in aux:
+        aux.add(num)
+        lista.append(num)
+        
+print("La lista con elementos únicos:")
+print(lista)
