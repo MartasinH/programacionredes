e9b4736f85b50cf2e935f8e49b5aa32f6d6fca8f
MartasinH <lugom15122@gmail.com>
2024-10-02 19:17:42 -0600

    MODULO 4

---
+++ Unidad 1/Modulo4/4.3.1.9 Números primos ¿Cómo encontrarlos.py
@@ -0,0 +1,13 @@
+'''Autor:Marta Lugo López
+Fecha de entrega:02/oct/2024'''
+
+def is_prime(num):
+    for i in range(2, int(1 + num ** 0.5)):
+        if num % i == 0:
+            return False
+    return True
+
+for i in range(1, 20):
+    if is_prime(i + 1):
+        print(i + 1, end=" ")
+print()
