faa5e6df4bb077c5f87fb34e0e32985a8cc4484e
MartasinH <lugom15122@gmail.com>
2024-10-03 14:50:13 -0600

    Ejercicio 1

---
+++ Unidad 1/CALIZ/U1Ejer01_Marta.py
@@ -0,0 +1,53 @@
+'''
+Descripcion del scrip:Declarar variables
+Nombre:Marta Lugo López
+Fecha:03/Oct/2024
+Tema:Fundamentos de Python
+'''
+#Escribe la instrucción para desplegar el título “Ejercicio 1 – Fundamentos de python 
+eje = ("Ejercicio 1 – Fundamentos de python")
+print(eje)
+
+#Escribe la instrucción para desplegar el título “Autor – [Nombre del participante]”
+autor= ("Autor – Marta Lugo López" )
+print (autor )
+#Escribe la instrucción para realizar un salto de línea
+print( )
+
+#Declara una variable de tipo real y asigna el valor 4
+
+v1 = 4
+#Declara una variable de tipo real y asigna el valor 3
+v2 = 3 
+
+#Imprime el contenido de las dos variables
+print("El valor de  la primera variable es " )
+print(v1)
+
+print("El valor de  la segunda variable es " )
+print(v2)
+#Escribe la instrucción para realizar un salto de línea
+print ()
+
+# Haz el cálculo de la suma y asígnala a una variable llamada suma; haz lo mismo para las operaciones de resta, multiplicación, división y potenciación
+
+print("Suma")
+suma = (v1 + v2 )
+print("La suma es ", suma )
+
+print("Resta")
+resta = (v1 - v2 )
+print("La resta es ", resta )
+
+print("Multiplicación ")
+Multi = (v1 * v2 )
+print("La Multiplicacion es ", Multi )
+
+print("Divición ")
+Div = (v1 / v2 )
+print("La División es ", Div )
+
+print("Potencia ")
+Pot = (v1**v2)
+print("La Potencia es ", Pot )
+
