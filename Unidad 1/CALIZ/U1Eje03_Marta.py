04d6ce684b5194699c68bc825434148e19bbe3bf
MartasinH <lugom15122@gmail.com>
2024-10-03 20:36:59 -0600

    Modelo 4

---
+++ Unidad 1/CALIZ/U1Eje03_Marta.py
@@ -0,0 +1,10 @@
+'''
+Descripcion del scrip:El participante aprenderá a utilizar y manipular el manejo de colecciones y
+listas en el lenguaje de programación Python
+Nombre:Marta Lugo López
+Fecha:03/Oct/2024
+Tema:Manejo de colecciones
+'''
+
+GIR0641 =()
+ numeros_control 
