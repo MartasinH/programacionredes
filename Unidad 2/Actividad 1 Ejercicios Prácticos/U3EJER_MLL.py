34e8ec6a8601ac8389ebe0e5f06b911b4b6a2070
MartasinH <lugom15122@gmail.com>
2024-11-03 11:51:03 -0600

    Api 3

--- Unidad 2/Actividad 1 Ejercicios Prácticos/U3EJER_MLL.py
+++ Unidad 2/Actividad 1 Ejercicios Prácticos/U3EJER_MLL.py
@@ -1,7 +1,8 @@
 import requests
 import json
-import time
 
+
+#Api de fotos de la Nasa
 url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY"
 
 payload = {}
@@ -9,31 +10,19 @@ headers = {
     'Content-Type': 'application/json',
     'x-api-key': 'Y0ZH5MkDIf019Q701rv9vGd2KNOBn9nPfRk7Fx1B'
 }
+opciones = {
+    12: "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631260503686E03_DXXX.jpg",
+    7: "http://mars.jpl.nasa.gov…631290305226E03_DXXX.jpg",
+    28:"http://mars.jpl.nasa.gov…631180503678E01_DXXX.jpg"
+}
+print("----------------------------------------------------------------------------------------------")
 
-while True:
-    print("*************************")
-    camera = input("Ingresa el nombre de la cámara \n - FHAZ \n - RHAZ \n - MAST \n  o 'salir' para finalizar: ")
-    print("**************************")
-    if camera.lower() == 'salir' or camera.lower() == 's': 
-        break 
-    request_url = f"{url}&camera={camera.upper()}"
-    print(f"Solicitando imágenes desde: {request_url}")
+# Pedir al usuario que seleccione una opción
+opcion = int(input("Sorteo de imágenes \n - Escoge un número \n - 12 \n - 7 \n - 28 \n 'Introduce :' "))
 
-    response = requests.get(request_url)
-    if response.status_code == 200:
-        json_data = response.json() 
-        photos = json_data.get('photos', [])
+if opcion in opciones:
+    print(f"La imagen seleccionada es: {opciones[opcion]}")
+else:
+     print("Byee")
 
-        if photos:  
-            print(f"Se encontraron {len(photos)} imágenes para la cámara {camera.upper()}:")
-            for idx, photo in enumerate(photos[:5], 1):  #  solo las primeras 5 imágenes
-                print(f"{idx}. {photo['img_src']}")
-        else:
-            print(f"No se encontraron imágenes para la cámara {camera.upper()}.")
-    elif response.status_code == 401:
-        print("Usuario no autorizado. Verifique la clave API.")
-    else:
-        print(f"Error: código {response.status_code}, no se pudo obtener las imágenes.")
-    time.sleep(3)
-
-print("Byee")
+   
